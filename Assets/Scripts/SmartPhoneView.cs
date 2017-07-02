1b5c57af2eb77ef3dce77b84ce5c8f1b3b5bcd19
HenanXiao4 <wingrakhi@live.jp>
2017-07-03 06:33:21 +0900

    ゲーム内画面の表示

---
+++ Assets/Scripts/SmartPhoneView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SmartPhoneView : MonoBehaviour
+{
+	[SerializeField] private Text timeText;
+	
+	private DateTime now;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timeText.text = DateTime.Now.ToString();
+	}
+}
