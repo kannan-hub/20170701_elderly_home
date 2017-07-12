6203b8cdc92dbc63a8d58c78a5acfa1ddb1ace43
HenanXiao4 <wingrakhi@live.jp>
2017-07-12 21:51:30 +0900

    smartphone更新

--- Assets/Scripts/SmartPhoneView.cs
+++ Assets/Scripts/SmartPhoneView.cs
@@ -3,20 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 
 public class SmartPhoneView : MonoBehaviour
 {
+	private const float BatteryConsumeSeconds = 1.0f;
+
 	[SerializeField] private Text timeText;
+	[SerializeField] private Text batteryRateText;
+	[SerializeField] private Image batteryRateImage;
+	[SerializeField] private ReactiveProperty<float> batteryRate = new ReactiveProperty<float>(1.0f);
 	
 	private DateTime now;
 	
 	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update ()
+	void Start ()
 	{
-		timeText.text = DateTime.Now.ToString();
+		string todayTime = DateTime.Now.ToShortTimeString();
+		timeText.text = todayTime;
+
+		Observable.Interval(TimeSpan.FromMinutes(1))
+			.Subscribe(_ =>
+			{
+				todayTime = DateTime.Now.ToShortTimeString();
+				timeText.text = todayTime;
+			}).AddTo(this);
+
+		Observable.Interval(TimeSpan.FromSeconds(BatteryConsumeSeconds))
+			.Subscribe(_ => batteryRate.Value -= 0.01f).AddTo(this);
+
+		batteryRate.Subscribe(rate =>
+		{
+			batteryRateText.text = rate.ToString("P0");
+			batteryRateImage.rectTransform.localScale =
+				new Vector3(rate, batteryRateImage.transform.localScale.y, batteryRateImage.transform.localScale.z);
+		}).AddTo(this);
 	}
 }
---
+++ Assets/Scripts/UseTwitter/AccecssTwitter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Twitter;
+using UnityEngine;
+
+namespace UseTwitter
+{
+	/// <summary>
+	/// http://qiita.com/toofu/items/075efed6ab1f23e94388
+	/// </summary>
+	public class AccecssTwitter : MonoBehaviour {
+		void Awake () {
+			Oauth.consumerKey       = "sy0H1Dzml4mjWTnWKGLzzDbNY";
+			Oauth.consumerSecret    = "XxU87c6bo1prODrKVL9rRuFLwRUwxaX2phKnmx9rK1ZMu1luHc";
+			Oauth.accessToken       = "45102673-mzOwGWMZcsxUqvVaYs7tFgjWpTv7DSHz8X8iOyJ84";
+			Oauth.accessTokenSecret = "dbNWtiUTn4j0P4RqoObMVOh2mBQUsykYXHcZdbe2DZ6Cn";
+		}
+		
+		void Start() {
+			Dictionary<string, string> parameters = new Dictionary<string, string>();
+			parameters ["status"] = "Tweet from Unity";  // ツイートするテキスト
+			StartCoroutine (Client.Post ("statuses/update", parameters, Callback));
+		}
+
+		void Callback(bool success, string response) {
+			if (success) {
+				Tweet tweet = JsonUtility.FromJson<Tweet> (response); // 投稿したツイートが返ってくる
+			} else {
+				Debug.Log (response);
+			}
+		}
+	}
+}
