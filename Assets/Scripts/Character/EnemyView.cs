e6c4d9c591ac2a59522276591ff5946781762c9d
HenanXiao4 <wingrakhi@live.jp>
2017-10-07 17:04:26 +0900

    敵配置と触れたらタヒる

---
+++ Assets/Scripts/Character/EnemyView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Character
+{
+    /// <summary>
+    /// 敵キャラの表示に関すること
+    /// パラメーターなど
+    /// </summary>
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class EnemyView : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 10f)]
+        private float walkingSpeed;
+
+        //未実装だよ、敵の要素実装時に決めるよ
+//        [SerializeField]
+//        private float runningSpeed;
+        
+        [HideInInspector]
+        private NavMeshAgent agent;
+
+        private void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.speed = walkingSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Character/PlayerView.cs
@@ -0,0 +1,67 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace Character
+{
+    /// <summary>
+    /// プレイヤー表示に関すること
+    /// </summary>
+    public class PlayerView : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 10f)]
+        private float forwardSpeed;
+
+        [SerializeField, Range(0f, 10f)]
+        private float backwardSpeed;
+
+        [SerializeField, Range(0f, 10f)]
+        private float strafeSpeed;
+
+        [SerializeField, Range(0f, 5f)]
+        private float runMultiplier;
+
+        [HideInInspector]
+        private RigidbodyFirstPersonController controller;
+
+        private void Awake()
+        {
+            controller = GetComponent<RigidbodyFirstPersonController>();
+            controller.movementSettings = new RigidbodyFirstPersonController.MovementSettings
+            {
+                ForwardSpeed = forwardSpeed,
+                BackwardSpeed = backwardSpeed,
+                StrafeSpeed = strafeSpeed,
+                RunMultiplier = runMultiplier
+            };
+        }
+
+        //TODO:パラメーターと動作状態は分割すべきかと
+
+        private readonly ReactiveProperty<bool> exsist = new BoolReactiveProperty(true);
+
+        /// <summary>
+        /// 生存しているかどうか
+        /// </summary>
+        public IObservable<bool> Exsist
+        {
+            get { return exsist; }
+        }
+
+        [SerializeField]
+        private ObservableCollisionTrigger collisionTrigger;
+
+        private void Start()
+        {
+            collisionTrigger.OnCollisionEnterAsObservable().Subscribe(colliion =>
+            {
+                if (colliion.transform.CompareTag("Enemy"))
+                {
+                    Debug.Log("DEAD!!!");
+                    //Destroy(this);
+                }
+            }).AddTo(this);
+        }
+    }
+}
